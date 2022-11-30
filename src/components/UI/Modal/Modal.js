9a4deff56aaf3c5a17de75e16e9a1c52763aeb56
Sodey Haidor Gabriel <sodeygabriel123@gmail.com>
2022-11-30 09:58:35 +0000

    first commit

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Auxiliary>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
+class Modal extends Component {
+
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !==this.props.show;
+    }
+
+    componentWIllUpdate () {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render () {
+        return (
+            <Auxiliary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100)',
-                opacity: props.show ? '1': '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}>
-            {props.children}
+            {this.props.children}
         </div>
     </Auxiliary>
-);
+        );
+    }
+}
 
-export default modal;
+export default Modal;
